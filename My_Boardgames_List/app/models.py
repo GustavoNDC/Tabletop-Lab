ffc677b67c90ab660a71e09319e14327faccbdb0
GustavoNDC <127903496+GustavoNDC@users.noreply.github.com>
2025-06-24 18:17:30 -0300

    Update models.py

--- My_Boardgames_List/app/models.py
+++ My_Boardgames_List/app/models.py
@@ -5,7 +5,7 @@ class Usuario(db.Model):
     __tablename__ = 'usuario'
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(120), unique=True, nullable=False)
-    senha = db.Column(db.String(120), nullable=False)
+    senha = db.Column(db.String(255), nullable=False)
     
     # Define a senha, gerando o hash e atribuindo ao campo senha
     def set_password(self, senha):
